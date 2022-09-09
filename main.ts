82793facd7d233f9d2f4e88f14861af773fe4c3f
Max Jung <max@gourmetjs.com>
2022-09-09 12:41:47 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,16 +6,27 @@ setInterval(() => {
   cur_ticks++
 }, 1000);
 
-serve(req => {
-  let dir = Deno.cwd()
+function get_headers(req: Request, indent = "  ") {
   let headers: string[] = []
-
   for (let [k, v] of req.headers.entries()) {
     headers.push(`${k}: ${v}`)
   }
+  return headers.map(s => indent + s).join("\n")
+}
 
+function get_env(indent = "  ") {
+  let env: string[] = []
+  for (let [k, v] of Object.entries(Deno.env.toObject())) {
+    env.push(`${k}: ${v}`)
+  }
+  return env.map(s => indent + s).join("\n")
+}
+
+serve(req => {
+  let dir = Deno.cwd()
   let content = [
-    `headers:\n${headers.map(s => "  " + s).join("\n")}`,
+    `headers:\n${get_headers(req)}`,
+    `env:\n${get_env()}`,
     `Deno.cwd: ${dir}`,
     `cur_ticks: ${cur_ticks}`
   ].join("\n");
